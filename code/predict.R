90003cc9aa5dd013d8c1ebbcd5e5818e9998d39a
André das Trevas <atrevas@gmail.com>
2015-03-15 12:06:47 -0300

    Remove variables with lots of NAs

--- code/predict.R
+++ code/predict.R
@@ -6,7 +6,7 @@ fn <- file.path('data', 'pml-training.csv')
 
 # Load the training data
 wle_raw <- read.csv(fn, stringsAsFactor = FALSE, na.strings = c('NA', ''), quote = '\"')
-wle_raw <- tbl_df(wle)
+wle_raw <- tbl_df(wle_raw)
 
 # How many observation and variables we have
 d <- dim(wle_raw)
@@ -16,11 +16,17 @@ df
 # Take a first look at the variables
 glimpse(wle_raw)
 
+# Get the percentage of NAs values in each variable
+perc_nas <- sapply(wle_raw, function (x) sum(is.na(x)) / length(x))
 
-sapply(wle_raw, function (x) sum(is.na(x)) / length(x))
+# Get a list of variables with more than 90% of NAs values
+high_nas <- perc_nas[perc_nas > 0.9]
+names(high_nas)
 
+# Remove the hight NAs variables
+wle_clean <- wle_raw %>%
+  select(-one_of(names(high_nas)))
 
+# Take a first look at the variables
+glimpse(wle_clean)
 
-wle %>%
-  select(user_name) %>%
-  distinct()
