1b72146dce9c6b3d9a1ed548d494f3e5927273c5
André das Trevas <atrevas@gmail.com>
2015-03-18 21:26:37 -0300

    Change variables names

--- code/predict.R
+++ code/predict.R
@@ -11,26 +11,26 @@ library(lubridate)
 fn <- file.path('data', 'pml-training.csv')
 
 # Load the training data
-wle_raw <- read.csv(fn, stringsAsFactor = FALSE, na.strings = c('NA', '')
+raw <- read.csv(fn, stringsAsFactor = FALSE, na.strings = c('NA', '')
                     , quote = '\"')
-wle_raw <- tbl_df(wle_raw)
+raw <- tbl_df(raw)
 
 ###############################################################################
 # First look at the raw data
 ###############################################################################
 # How many observation and variables we have
-d <- dim(wle_raw)
+d <- dim(raw)
 df <- data.frame(Obs = d[1], Vars = d[2])
 df
 
 # Take a first look at the variables
- glimpse(wle_raw)
+glimpse(raw)
 
 ###############################################################################
 # Remove variables with lots of NAs
 ###############################################################################
 # Get the percentage of NAs values in each variable
-perc_nas <- sapply(wle_raw, function (x) sum(is.na(x)) / length(x))
+perc_nas <- sapply(raw, function (x) sum(is.na(x)) / length(x))
 
 # Get a list of variables with more than 90% of NAs values
 high_nas <- perc_nas[perc_nas > 0.9]
@@ -38,40 +38,44 @@ high_nas <- perc_nas[perc_nas > 0.9]
 # names(high_nas)
 
 # Remove the hight NAs variables
-wle_clean <- wle_raw %>%
+train <- raw %>%
   select( - one_of(names(high_nas)))
 
 ###############################################################################
 # Transform original variables
 ###############################################################################
 # Create a date column
-wle_clean <- wle_clean%>%
+train <- train %>%
   mutate(wle_date = dmy_hm(cvtd_timestamp))
 
 # Remove the original timestamp variable
-wle_clean <- wle_clean %>%
+train <- train %>%
   select(- cvtd_timestamp)
 
 # Define factor variables
-wle_clean <- wle_clean %>%
+train <- train %>%
   mutate(user_name = factor(user_name)
          , new_window = factor(new_window)
          , classe = factor(classe))
 
+# Create list of factor variables
+is_factor <- sapply(train, is.factor)
+factors <- is_factor[is_factor == TRUE]
+
 # Take a first look at the variables
-glimpse(wle_clean)
+glimpse(train)
 
 # Check which variables are numeric
-#is_num <- sapply(wle_clean, is.numeric)
-#num <- names(is_num[is_num == TRUE])
-#non_num <- names(is_num[is_num == FALSE])
+is_num <- sapply(train, is.numeric)
+numerics <- names(is_num[is_num == TRUE])
+
 
 
 ###############################################################################
 # Plot some bar graphs
 ###############################################################################
 # Create a data frame for bar graphs
-wle_bar <- wle_clean %>%
+bar <- train %>%
   select(user_name, new_window, classe, wle_date) %>%
   mutate(
     user_name = factor(user_name)
@@ -80,13 +84,13 @@ wle_bar <- wle_clean %>%
     , wle_date = as.Date(wle_date)
     )
 
-# glimpse(wle_bar)
+# glimpse(bar)
 
 # Specify breaks as a Date vector
-date_breaks <- seq(as.Date(min(wle_clean$wle_date)), as.Date(max(wle_clean$wle_date)), by = '1 day')
+date_breaks <- seq(as.Date(min(train$wle_date)), as.Date(max(train$wle_date)), by = '1 day')
 
 # Build a bar graph showing the number of observation by the user_name variable
-wle_bar %>%
+bar %>%
   group_by(wle_date, user_name) %>%
   summarise( count = n()) %>%
   ggplot(aes(x = wle_date, y = count, fill = user_name)) +
@@ -98,7 +102,7 @@ wle_bar %>%
     ggtitle('Number of observations by date and user_name\n')
     
 # Build a bar graph showing the number of observations by the classe variable
-wle_bar %>%
+bar %>%
   group_by(wle_date, classe) %>%
   summarise( count = n()) %>%
   ggplot(aes(x = wle_date, y = count, fill = classe)) +
@@ -110,7 +114,7 @@ wle_bar %>%
     ggtitle('Number of observations by date and classe\n')
 
 # Build a bar graph showing the number of observations by the new_window variable
-wle_bar %>%
+bar %>%
   group_by(wle_date, new_window) %>%
   summarise( count = n()) %>%
   ggplot(aes(x = wle_date, y = count, fill = new_window)) +
@@ -124,20 +128,20 @@ wle_bar %>%
 ###############################################################################
 # Create dummy variables
 ###############################################################################
-dummies <- dummyVars(classe ~ ., data = wle_clean)
-
-wle_clean <- tbl_df(as.data.frame(predict(dummies, newdata = wle_clean)))
+# dummies <- dummyVars(~ user_name + new_window , data = train)
+# 
+# glimpse(tbl_df(as.data.frame(predict(dummies, newdata = train))))
 
 
 ###############################################################################
 # Remove zero and near zer-variance predictors
 ###############################################################################
-dim(wle_clean)
-nzv <- nearZeroVar(wle_clean)
-wle_clean <- wle_clean %>%
-  select(-nzv)
-dim(wle_clean)
+# dim(wle_clean)
+# nzv <- nearZeroVar(wle_clean)
+# wle_clean <- wle_clean %>%
+#   select(-nzv)
+# dim(wle_clean)
 
 
-pre_pro <-  preProcess(wle_clean %>% select(-classe), method = c("center", "scale"))
-predict(pre_pro, wle)
+pre_pro <-  preProcess(train[, numerics], method = c("center", "scale"))
+tbl_df(data.frame(predict(pre_pro, train[, numerics])))
